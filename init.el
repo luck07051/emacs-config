8617a5d216c9cf7ba2b31934b55ab0bdce0d4ab8
luck07051 <luck0751@gmail.com>
2023-12-01 03:26:06 +0800

    tangle

--- init.el
+++
@@ -1,9 +0,0 @@
-(org-babel-load-file
- (expand-file-name
-  "config/config.org"
-  user-emacs-directory))
-(org-babel-load-file
- (expand-file-name
-  "config/org-mode.org"
-  user-emacs-directory))
-(setq custom-file "~/.config/emacs/custom.el")
